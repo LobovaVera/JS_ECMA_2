3c9428ba736af38d39da1ae5145b2b2f67e342d8
Vera <pchilli@yandex.ru>
2024-01-29 19:09:44 +0300

    hw done

--- 1.js
+++ 1.js
@@ -9,3 +9,16 @@
 // Свойство pages (количество страниц) - число, количество страниц в книге.
 // Метод displayInfo() - выводит информацию о книге (название, автор и количество страниц).
 
+class Book {
+    constructor(title, author, pageNumber){
+        this.title = title;
+        this.author = author;
+        this.pageNumber = pageNumber;
+    }
+    displayInfo(){
+        console.log(`Эта книга ${this.title}, ее написал ${this.author}, в ней ${this.pageNumber} страниц`)
+    }
+}
+
+const newBook = new Book("Сто лет одиночества", "Маркес", 230);
+newBook.displayInfo();
--- 2.js
+++ 2.js
@@ -21,3 +21,18 @@
 // // Name: Jane Doe
 // // Age: 17
 // // Grade: 11th grade"
+
+class Student {
+    constructor(name, age, grade){
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+    }
+    displayInfo(){
+        console.log(`Name: ${this.name}\nAge: ${this.age}\nGrade: ${this.grade}`);
+    }
+}
+const student1 = new Student("John Smith", 16, "10th grade");
+student1.displayInfo();
+const student2 = new Student("Jane Doe", 17, "11th grade");
+student2.displayInfo();
